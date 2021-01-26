0aed938de0e8cd3dd92c4db3df4799a0aa8f289d
Sam Sheets <sam.sheets94@gmail.com>
2021-01-26 16:05:37 -0700

    added config file

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://samsheets@localhost/recipenest',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://samsheets@localhost/recipenest-test'
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://samsheets@localhost/recipenest-test',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api'
 }
