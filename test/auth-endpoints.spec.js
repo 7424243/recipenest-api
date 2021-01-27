3f58133c6f36a385e18c42b37dfd7cda7d5aecb0
Sam Sheets <sam.sheets94@gmail.com>
2021-01-27 16:36:39 -0700

    commented and cleaned up auth tests

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -6,11 +6,14 @@ const {makeUsersArray} = require('./users.fixtures')
 const jwt = require('jsonwebtoken')
 
 describe(`Auth Endpoints`, () => {
-    let db
 
+    //testing data
     const testUsers = makeUsersArray()
     const testUser = testUsers[0]
+    const protectedUsers = hashUserPassword(testUsers)
 
+    //create a connection to the test db
+    let db
     before('make knex instance', () => {
         db = knex({
             client: 'pg',
@@ -19,6 +22,7 @@ describe(`Auth Endpoints`, () => {
         app.set('db', db)
     })
 
+    //disconnect from database so that tests don't hang
     after('disconnect from db', () => db.destroy())
 
     //clear any data so that we have a fresh start
@@ -28,47 +32,66 @@ describe(`Auth Endpoints`, () => {
     afterEach('cleanup', () => db.raw('TRUNCATE recipenest_users RESTART IDENTITY CASCADE'))
    
     describe(`POST /api/auth/login`, () => {
+
         beforeEach('insert users', () => {
-            const protectedUsers = hashUserPassword(testUsers)
             return db
                 .into('recipenest_users')
                 .insert(protectedUsers)
         })
+
+        //testing for required fields
         const requiredFields = ['user_name', 'password']
         requiredFields.forEach(field => {
             const loginAttemptBody = {
                 user_name: testUser.user_name,
                 password: testUser.password,
             }
+
             it(`responds with 400 required error when '${field}' is missing`, () => {
+                
                 delete loginAttemptBody[field]
+                
                 return supertest(app)
                     .post('/api/auth/login')
                     .send(loginAttemptBody)
                     .expect(400, {
                         error: `Missing '${field}' in request body`
                     })
+
             })
         })
+
+        //test for invalid user_name
         it(`responds 400 'invalid user_name or password' when bad user_name`, () => {
+            
             const userInvalidUsername = {user_name: 'user-not', password: testUser.password}
+            
             return supertest(app)
                 .post('/api/auth/login')
                 .send(userInvalidUsername)
                 .expect(400, {error: `Incorrect user_name or password`})
         })
+
+        //test for invalid password
         it(`responds 400 'invalid user_name or password' when bad password`, () => {
+            
             const userInvalidPassword = {user_name: testUser.user_name, password: 'wrong'}
+            
             return supertest(app)
                 .post('/api/auth/login')
                 .send(userInvalidPassword)
                 .expect(400, {error: `Incorrect user_name or password`})
         })
+
+        //test for happy path
         it(`responds 200 and JWT auth token using secret when valid credentials`, () => {
+            
+            
             const userValidCreds = {
                 user_name: testUser.user_name,
                 password: testUser.password,
             }
+
             const expectedToken = jwt.sign(
                 {user_id: testUser.id}, 
                 process.env.JWT_SECRET, {
@@ -76,13 +99,16 @@ describe(`Auth Endpoints`, () => {
                     algorithm: 'HS256'
                 }
             )
+
             return supertest(app)
                 .post('/api/auth/login')
                 .send(userValidCreds)
                 .expect(200, {
                     authToken: expectedToken
                 })
+
         })
+
     })
 
 })
