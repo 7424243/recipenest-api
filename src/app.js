abd14e6941144285ffd11fccc31a8131f2c6b412
Sam Sheets <sam.sheets94@gmail.com>
2021-03-24 14:12:13 -0600

    changed configurable cors to *

--- src/app.js
+++ src/app.js
@@ -14,17 +14,17 @@ const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common'
 
 app.use(morgan(morganOption))
 app.use(helmet())
-//app.use(cors())
+app.use(cors())
 
 //allow CORS for dynamic origins
-const whitelist = ['http://localhost:3000', 'https://recipenest.vercel.app']
-const corsOptions = {
-    origin: function(origin, callback) {
-        let originWhitelisted = whitelist.indexOf(origin) !== -1
-        callback(null, originWhitelisted)
-    }
-}
-app.use(cors(corsOptions))
+// const whitelist = ['http://localhost:3000', 'https://recipenest.vercel.app']
+// const corsOptions = {
+//     origin: function(origin, callback) {
+//         let originWhitelisted = whitelist.indexOf(origin) !== -1
+//         callback(null, originWhitelisted)
+//     }
+// }
+//app.use(cors(corsOptions))
 
 app.use('/api/recipes', recipesRouter)
 app.use('/api/users', usersRouter)
