87a8ea97ebec9731867eb80349d38eb1a1d36a59
Sam Sheets <sam.sheets94@gmail.com>
2021-03-24 14:24:40 -0600

    attempt at enabling cors

--- src/app.js
+++ src/app.js
@@ -16,6 +16,13 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  })
+
 //allow CORS for dynamic origins
 // const whitelist = ['http://localhost:3000', 'https://recipenest.vercel.app']
 // const corsOptions = {
